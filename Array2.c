74a524cbc04a852d5e31e68fe1d8354b7bcb9c8c
James Fraser <wulfgar.pro@gmail.com>
2013-08-12 15:22:50 +1000

    added some more code for functions and arrays

---
+++ Array2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define SIZE 100
+
+/*
+ * This program prints a word size histogram horizontally.
+ */
+int main() {
+    int c, i, j, charCount = 0, word_number = 0, word[SIZE];
+
+    while ((c = getchar()) != EOF) {
+        if (c != ' ' && c != '\t' && c != '\n') {
+            for(i = 0; i < 4; i++){
+                if(words[i] == c){
+
+                }
+            }
+        }
+        else {
+            word[word_number] = charCount;
+            charCount = 0;
+            ++word_number;
+        }
+
+    }
+
+    for (i = 0; i < word_number; ++i) {
+        printf("%d: ", i+1);
+        for (j = 0; j < word[i]; ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
