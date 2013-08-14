1b4feec974f41d100e8e54a7f00ca9ce22e29427
James Fraser <wulfgar.pro@gmail.com>
2013-08-14 17:01:13 +1000

    added comments for compiling linked .c files

--- Function2_a.c
+++ Function2_a.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/*
+ * Compile with gcc -c
+ */
 int add(int a, int b) {
     return a + b;
 }
--- Function2_b.c
+++ Function2_b.c
@@ -3,6 +3,11 @@
 // function prototype for use in this source file # see Function2_a.c
 int add(int a, int b);
 
+/*
+ * Compile with gcc -c
+ * 
+ * After compiling Function2_a.c and this .c file, link with gcc Function2_a.o Function2_b.o -o program
+ */
 int main(void){
     int a = 1;
     int b = 2;
