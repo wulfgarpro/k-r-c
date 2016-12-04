a820885fcbd8d45d5e51bcced7922a507f4e3fb0
James Fraser <wulfgar.pro@gmail.com>
2016-12-04 17:58:49 +1100

    finished impl for 2-6

--- 2_6_setbits.c
+++ 2_6_setbits.c
@@ -4,8 +4,18 @@
  * Set x bits p->n as right most n bits of y
  */
 int setbits(int x, int p, int n, int y) {
-    int mask = ~((~0) << n);
-    return ((mask & y) << p) | ((~(mask) << p) & x);
+    /*int mask = ~((~0) << n);
+    printf("%d\n", mask); // 00000001
+    int mask_a = ((mask & y) << p);
+                                   
+    printf("%d\n", mask_a);
+    int mask_b = (~(mask) << p) & x; 
+                                    
+    printf("%d\n", mask_b);
+    return mask_a | mask_b;*/
+
+    int bitmask = ~((~0) << n);
+    return ((bitmask & y) << p) | (~(bitmask << p) & x);
 }
 
 int main(void) {
@@ -14,8 +24,7 @@ int main(void) {
     int p = 2;
     int n = 1;
 
-    // expect: 00000100 (4)
-
+    // expect: 00000001 (1)
     printf("%d\n", setbits(x,p,n,y));
     return 0;
 }
