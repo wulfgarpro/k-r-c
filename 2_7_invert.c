aee198d0ff0573274c834f5d3cd348091512bdc5
James Fraser <wulfgar.pro@gmail.com>
2016-12-07 20:00:00 +1100

    renamed and added clear comment

--- 2_7_invert.c
+++ 2_7_invert.c
@@ -5,9 +5,14 @@ unsigned int setbits(int x, int p, int n) {
 }
 
 int main(void) {
-    int x = 5; // 00000101 
-    int p = 3; // 00001011 // 11
+    int x = 5;
+    int p = 3; 
     int n = 3;
+
+    // 00000101
+    // 00001110
+    // XOR
+    // 00001011 // 11 
 
     printf("%d\n", setbits(x,p,n));
     return 0;
